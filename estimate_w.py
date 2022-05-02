2e00cd301e5daa806378ea21bc6ce6725915165a
JakubReha <kubareha107@gmail.com>
2022-05-02 11:27:26 +0200

    estimate_w should work

--- estimate_w.py
+++ estimate_w.py
@@ -1,40 +1,31 @@
 import numpy as np
-from torch.utils.data import Dataset, DataLoader
+from dataloaders import create_dataloader
+from tqdm import tqdm
+from scipy.ndimage import gaussian_filter1d
 
-
-def CalculateW(train_dataloader):
-    """Calculates the weights w which are used in the loss function,
-    dangerously unfinished at this moment"""
-    # For every image in training set:
-    #   There's probably a better way to do it
-    #   calculate which bin the color belongs to per pixel
-    #   add that to a [Q,H,W] counter matrix, +1 if it belongs etc.
-    #   (might be better to do it some other way tho)
-    # Divide by the number of images to get the empirical dist.
-    # Smooth the distribution with a gaussian kernel
-    # Mix it with a uniform distribution
-    # Normalize and save w as a [Q,H,W] matrix
-
-    lamb = 1 / 2
+def CalculateSaveW():
+    """Class rebalancing"""
+    train_dataloader = create_dataloader(64, 224, False, "val", "tree.p")
+    lamb = 0.5
     sigma = 5
 
-    # image dimension
-    p = np.zeros([322, 256, 256])
+    # ab color distribution (now int, but it will get transformed into the correct shape 1D)
+    p = 0
+    for x, y in tqdm(train_dataloader):
+        # y [batch_size, 322, 224, 224]
+        p += y.numpy().mean(axis=(0, 2, 3))
 
-    for x, y in train_dataloader:
-        # y contains the closest bin in ab space for each pixel (like one-hot encoded)
-        # Must do some transform to get that :^)
-        p += y.numpy()[0, :, :, :]
+    p /= len(train_dataloader)
 
-    p /= len(training_data)
-
-    # TODO: Gaussian kernel here please, will probably have to go to 2d space instead of binned space?
-
-    # calculate w
-    w = np.reciprocal((1 - lamb) * p + lamb / p.shape[0])
+    # smooth with gaussian filter
+    p_smooth = gaussian_filter1d(p, sigma)
+    # mix with uniform
+    w = 1/((1 - lamb) * p_smooth + lamb / p.shape[0])
 
     # normalize
-    for i in range(256):
-        for j in range(256):
-            w[:, i, j] /= np.sum(p[:, i, j] * w[:, i, j])
-    return w
+    w = w / np.dot(w, p)
+    np.save("W.npy", w)
+
+
+if __name__ == '__main__':
+    CalculateSaveW()
