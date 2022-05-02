3dfc49acd3c157ff48258a2542335c15f7d74719
JakubReha <kubareha107@gmail.com>
2022-05-02 15:26:25 +0200

    bug fixes

--- main.py
+++ main.py
@@ -18,17 +18,19 @@ if __name__ == '__main__':
     loss = CustomLoss()
 
     for X, Weights, ii in val_loader:
-        x, y = encode(X, Weights, ii)
+        X, y = encode(X, Weights, ii)
+        del ii
+        del Weights
         pred = torch.rand((configs.batch_size, 322, configs.input_size, configs.input_size))
         J = loss(pred, y)
         _, axs = plt.subplots(8, 4, figsize=(12, 12))
         axs = axs.flatten()
-        for img, ax in zip(x, axs):
+        for img, ax in zip(X, axs):
             ax.imshow(img[0].detach().numpy(), cmap='gray')
         plt.show()
         _, axs = plt.subplots(8, 4, figsize=(12, 12))
         axs = axs.flatten()
-        for img, ax in zip(x, axs):
+        for img, ax in zip(X, axs):
             ax.imshow(img[1].detach().numpy(), cmap='gray')
         plt.show()
         break
