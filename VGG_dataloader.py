04b98506f703e1b51d846c1843318b63e0a770b6
Alex <alex.norlin.99@gmail.com>
2022-05-19 16:18:37 +0200

    VGG testing should be done now

--- VGG_dataloader.py
+++ VGG_dataloader.py
@@ -6,22 +6,40 @@ import os
 import glob
 from skimage import color
 from PIL import Image
-import csv
-
-
+import pickle
+from scipy.stats import norm
 
 class CustomDataSet(Dataset):
+    def __init__(self, main_dir, input_size, mode, tree_path):
+        self.images = glob.glob(os.path.join(os.path.join(main_dir, mode), '*.JPEG'))
+        self.mode = mode
+        self.input_size = input_size
+        self.n_neighbours = 5
+        self.q = 322
+        with open(tree_path, 'rb') as pickle_file:
+            self.tree = pickle.load(pickle_file)
+
+    def __len__(self):
+        return len(self.images)
+
+    def __getitem__(self, idx):
+        image = Image.open(self.images[idx]).convert('RGB')
+        rgb = transforms.Resize((self.input_size, self.input_size))(image)
+        rgb = transforms.RandomHorizontalFlip()(rgb)
+        lab = color.rgb2lab(rgb)
+        lab = lab.transpose([2, 0, 1])
+        dist, ii = self.tree.query(lab[1:3].reshape(2, self.input_size**2).T, self.n_neighbours)
+        x = torch.from_numpy(lab)
+        weights = norm.pdf(dist, loc=0, scale=5)
+        if self.n_neighbours > 1:
+            weights = weights / weights.sum(axis=-1)[:, None]
+        return torch.unsqueeze(x[0], 0), weights, ii, os.path.basename(self.images[idx]).split('_')[0]
+
+class CustomDataSetVGG(Dataset):
     def __init__(self, main_dir, input_size, mode, transform_extra):
         # Might want to create a new one that works on the output from our colorizer
         self.images = glob.glob(os.path.join(os.path.join(main_dir, mode), '*.JPEG'))
         self.mode = mode
-        d = {}
-        with open(main_dir + '/LOC_val_solution.csv', "r") as f:
-            categories = csv.reader(f)
-            next(categories, None)
-            for lines in categories:
-                d[lines[0]] = getnIDs(lines[1])
-        self.labels = d
         self.input_size = input_size
         self.transform_extra = transform_extra
         if transform_extra == 'normalize':
@@ -66,14 +84,23 @@ class CustomDataSet(Dataset):
             image = self.transforms(image)
         file_name = os.path.basename(self.images[idx])
         file_name_no_ext = os.path.splitext(file_name)[0]
-        return image, self.labels[file_name_no_ext]
+        return image, file_name_no_ext.split('_')[0]
 
-def create_dataloader(batch_size, input_size, shuffle, mode, transform_extra):
-    data = CustomDataSet("../ImageNet", input_size, mode, transform_extra)
+def create_dataloaderVGG(batch_size, input_size, shuffle, mode, transform_extra):
+    data = CustomDataSetVGG("../ImageNet", input_size, mode, transform_extra)
     data_loader = torch.utils.data.DataLoader(data,
                                               batch_size=batch_size,
                                               shuffle=shuffle,
                                               num_workers=1,
+                                              pin_memory=True)
+    return data_loader
+
+def create_dataloader_label(batch_size, input_size, shuffle, mode, tree_path):
+    data = CustomDataSet("../ImageNet", input_size, mode, tree_path)
+    data_loader = torch.utils.data.DataLoader(data,
+                                              batch_size=batch_size,
+                                              shuffle=shuffle,
+                                              num_workers=2,
                                               pin_memory=True)
     return data_loader
 
--- VGGnet.py
+++ VGGnet.py
@@ -1,11 +1,15 @@
 import numpy as np
 import torch
-from VGG_dataloader import create_dataloader
+from VGG_dataloader import create_dataloaderVGG, create_dataloader_label
+from dataloaders import encode
 from tqdm import tqdm
 from config import parse_configs
 from annealed_mean import pred_to_rgb_vec
+import torchvision.transforms as transforms
 from matplotlib import pyplot as plt
+from main import load
 import os
+from model import ConvNet
 
 def VGG_download(path):
     model = torch.hub.load('pytorch/vision:v0.10.0', 'vgg16', pretrained=True)
@@ -19,33 +23,64 @@ def VGG_eval(configs):
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     path = 'pretrained_VGG.pt'
     if not os.path.exists(path):
-        model = VGG_download(path)
+        VGGnet = VGG_download(path)
     else:
-        model = VGG_load(path)
+        VGGnet = VGG_load(path)
 
-    model.eval()
+    VGGnet.eval()
 
-    val_loader_VGG = create_dataloader(1, configs.input_size, True, "val", 'normalize')
-
-    # Have to download the imagenet_classes.txt to the VM if it isn't there
     with open('..\ImageNet\LOC_synset_mapping.txt', "r") as f:
         categories = [s.strip() for s in f.readlines()]
     categories = [x.split(" ")[0] for x in categories]
 
-    correct = 0
-    n_guesses = 0
-    for x,y in tqdm(val_loader_VGG, total=len(val_loader_VGG)):
-        with torch.no_grad():
-            output = model(x)
-        _, guess_catid = torch.topk(torch.nn.functional.softmax(output[0], dim=0), 1)
-        n_guesses += 1
-        #print(categories[guess_catid])
-        #plt.imshow(x[0].moveaxis(0,-1))
-        #plt.show()
-        if categories[guess_catid] in y[0]:
-            correct += 1
-        #print('Predicted category: ' + str(categories[guess_catid]) + '\n', 'True category: ' + str(y[0]) + '\n', 'Confidence: ' + str(guess_prob.item()) + '\n')
-    print('Total accuracy: ' + str(correct/n_guesses))
+    models_to_test = ['orig', 'random', 'grayscale','cars_full_35.tar', 'cars_no_weights_44.tar', 'cars_L2_64.tar']
+
+    accuracies = np.zeros(len(models_to_test))
+
+    for acc_index, model_name in enumerate(models_to_test):
+
+        if model_name == 'orig' or model_name == 'random' or model_name == 'grayscale':
+            val_loader = create_dataloaderVGG(1, configs.input_size, False, "sports_cars/val", model_name)
+            correct = 0
+            n_guesses = 0
+            for x, y in tqdm(val_loader, total=len(val_loader)):
+                with torch.no_grad():
+                    output = VGGnet(x)
+                _, guess_catid = torch.topk(torch.nn.functional.softmax(output[0], dim=0), 1)
+                n_guesses += 1
+                if categories[guess_catid] in y[0]:
+                    correct += 1
+            accuracies[acc_index] = correct/n_guesses
+            print(correct/n_guesses)
+
+        else:
+            val_loader = create_dataloader_label(1, configs.input_size, False, "sports_cars/val", "tree.p")
+            color_model = ConvNet().to(device)
+            color_model.to(torch.double)
+            optimizer_full = torch.optim.Adam(color_model.parameters(), lr=configs.lr, weight_decay=.001)
+            load(color_model, optimizer_full, model_name)
+            VGGnet.to(torch.double)
+
+            correct = 0
+            n_guesses = 0
+            color_model.eval()
+            with torch.no_grad():
+                for X, Weights, ii, label in tqdm(val_loader, leave=False):
+                    X, Z = encode(X, Weights, ii, device)
+                    del ii
+                    del Weights
+                    val_batch_Z = color_model(X)
+                    val_batch_im = pred_to_rgb_vec(X, val_batch_Z, device, T=0.38)
+                    hmm = transforms.Compose([transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                                                                          std=[0.229, 0.224, 0.225])])
+                    output = VGGnet(hmm(val_batch_im.to(torch.double)))
+                    _, guess_catid = torch.topk(torch.nn.functional.softmax(output[0], dim=0), 1)
+                    n_guesses += 1
+                    if categories[guess_catid] == label[0]:
+                        correct += 1
+                    accuracies[acc_index] = correct / n_guesses
+    for i in range(accuracies):
+        print(models_to_test[i] + ' accuracy: ' + str(accuracies[i]) + '\n')
 
 
 if __name__ == '__main__':
